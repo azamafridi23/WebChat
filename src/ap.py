d4d05e284b0ef33e5a0e537d76c1643ad02c052f
Azam <iamazamafridi@gmail.com>
2024-03-03 16:45:37 +0500

    Working Structure of the project is defined

--- src/ap.py
+++ src/ap.py
@@ -1,11 +1,23 @@
 import streamlit as st
 
-st.set_page_config(page_title="Chat With Websites",page_icon="")
+def get_response(user_input):
+    return "I dont know"
 
+# app config
+st.set_page_config(page_title="Chat With Websites",page_icon="")
 st.title("Chat With Websites")
 
+# user input
 with st.sidebar:
     st.header("Settings")
     website_url = st.text_input("Website URL")
 
-st.chat_input("Type your message here...")
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query!="":
+    response = get_response()
+    with st.chat_message("Human"):
+        st.write(user_query)
+
+    with st.chat_message("AI"):
+        st.write(response)
+
